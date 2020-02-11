7a17ed1f8e0f860d6d616e2f37a2e92ef105b58f
Jorden Tang <31497183+Jorden-Tang@users.noreply.github.com>
2020-02-10 20:48:34 -0800

    Update settings.py

--- online_shop_project/settings.py
+++ online_shop_project/settings.py
@@ -123,19 +123,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-
-# AWS_ACCESS_KEY_ID = 'AKIATCQN76NQCGXQEQ6L'
-# AWS_SECRET_ACCESS_KEY = '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'
-# AWS_STORAGE_BUCKET_NAME = 'online-shop-static'
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
-# AWS_LOCATION = 'static'
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'mysite/static'),
-# ]
-# # STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
